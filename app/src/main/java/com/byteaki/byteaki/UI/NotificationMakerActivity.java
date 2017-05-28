c25f15a9c18657224d59fd36965a29c4bd95cb2b
Bruno Botelho <brunorabelow@hotmail.com>
2017-05-28 02:08:45 -0300

    Adicionei dependencia ao build.gradle e ao NotificationMakerActivity

--- app/src/main/java/com/byteaki/byteaki/UI/NotificationMakerActivity.java
+++ app/src/main/java/com/byteaki/byteaki/UI/NotificationMakerActivity.java
@@ -4,12 +4,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.byteaki.byteaki.R;
+import com.firebase.client.Firebase;
 
+import java.util.HashMap;
+import java.util.Map;
 public class NotificationMakerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification_maker);
+        //
+    }
+
+
+    void sendNotification(String user,final String message){
+        Firebase ref = new Firebase("");
+        final Firebase notifications = ref.child("notificationRequests");
+
+        Map notification = new HashMap<>();
+        notification.put("username", user);
+        notification.put("message", message);
+
+        notifications.push().setValue(notification);
+
+
     }
 }
